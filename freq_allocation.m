c89dc616e64d0e464063a29116eba6c3b5182c23
Marino <mabrgr@gmail.com>
2015-07-30 15:48:23 +0200

    Removed a lot of code. Started almost from scratch

--- freq_allocation.m
+++ freq_allocation.m
@@ -1,13 +1,16 @@
 clear all
 
 % Number of APs
-NP = 5;
+NP = 4;
 
 % max distance between points
 MC = 20;
 
 % min distance
 Size = 100;
+
+%Channels to use
+availableFreqs = [1, 6, 11];
 
 Xmax = Size - 2*MC;
 Ymax = Size - 2*MC;
@@ -19,71 +22,10 @@ initialPlot(Px, Py, Size, NP)
 
 [neighbourlist,I] = sort(nlist,1,'ascend');
 
-modNeighbourlist = neighbourlist; %Neighbour list to be modified
-
 neighbourlist
 
 I
 
-d
-
-col_indexes = getColumnIndexes(modNeighbourlist, NP); %Get columnindexes for values with biggest value for inverse distance
-
-for i = col_indexes' %Removing indexes that are going to be used
-    modNeighbourlist(NP, i) = 0;
-end
-
 %Fiding the most critical node of the nodes chosen with shortest distance
-%(probably just two)
+%(probably just two) (Assume it is just two for now)
 invmeters = sumInverseMeters(col_indexes, neighbourlist, I);
-
-
-index = find(invmeters == max(invmeters));
-AP = col_indexes(index);
-col_indexes(index) = []; %Removing element
-
-availableFreqs = [1, 6, 11]; %This will not be changed
-modAvailableFreqs = availableFreqs; %This will change
-
-freq_alloc = zeros(1, NP);
-
-freq_alloc(AP) = modAvailableFreqs(randi(numel(modAvailableFreqs))); %Giving channel to most critical node
-modAvailableFreqs(modAvailableFreqs == freq_alloc(AP)) = []; %Remove channel picked
-
-if length(col_indexes) <= 2 %Assign frequency to the other APs (probably 1)
-    for i = 1:length(col_indexes)
-        AP = col_indexes(i);
-        freq_alloc(AP) = modAvailableFreqs(randi(numel(modAvailableFreqs)));
-        modAvailableFreqs(modAvailableFreqs == freq_alloc(AP)) = [];
-    end
-end
-%Kanskje legge inn funksjonalitet for > 2, men sjansen for at det skjer er
-%liten
-
-while length(find(freq_alloc)) < NP
-    modNeighbourlist = sort(modNeighbourlist,1,'ascend');
-    
-    col_indexes = getColumnIndexes(modNeighbourlist, NP);
-    
-    if length(find(freq_alloc(col_indexes))) == 1 %Only one of the APs don't have a channel assigned
-        for i = 1:length(col_indexes) 
-            if freq_alloc(col_indexes(i))
-                modNeighbourlist(NP, col_indexes(i)) = 0; %Remove element
-                col_indexes(i) = 0; %Remove elements that has already got a channel assigned
-            end
-        end
-        [~, ~, col_indexes] = find(col_indexes); %Returns vector with only non-zero elements
-        
-        if length(modAvailableFreqs) == 1
-            freq_alloc(col_indexes) = modAvailableFreqs;
-            modAvailableFreqs(1) = [];
-            
-        else
-            freq_alloc = chooseFrequency(freq_alloc, availableFreqs, col_indexes, I, neighbourlist);
-        end
-    end
-    break;
-    
-end
-
-%TODO: Finn neste nodepar og velg frekvenser for alle sammen.
