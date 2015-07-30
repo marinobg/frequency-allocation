680224fd724db7a32580b62b4fca868a1e55cda3
Marino <mabrgr@gmail.com>
2015-07-30 13:01:38 +0200

    Uses bestChannel

--- chooseFrequency.m
+++ chooseFrequency.m
@@ -1,25 +1,21 @@
 function freq_alloc = chooseFrequency(freq_alloc, availableFreqs, col_indexes, I, neighbourlist)
 
 if length(col_indexes) == 1 %Only one AP to assign
-    for i = length(neighbourlist):-1:1
-        if neighbourlist(i, col_indexes) == 0 %The next APs are out of range
-            break
-        end
-        AP = I(i, col_indexes);
-        if freq_alloc(AP) ~= 0
-            freq = freq_alloc(AP);
-            index = find(availableFreqs == freq);
-            availableFreqs(index) = []; %Removing channels that will interfere the most
-            
-            if length(availableFreqs) == 1 %If only one channel to choose left
-                break
-            end
-        end
-        
-    end
-    
+    col_indexes = nextAP;
     %Choose one of the available channels
-    freq_alloc(col_indexes) = availableFreqs(randi(numel(availableFreqs)));
+    freq_alloc(nextAP) = bestChannel(neighbourlist, nextAP, I, freq_alloc, availableFreqs);
     
+else
+    invmeters = sumInverseMeters(col_indexes, neighbourlist, I);
+    
+    while length(invmeters) > 0
+        index = find(invmeters == max(invmeters));
+        invmeters(index) = [];
+        nextAP = col_indexes(index); %AP to get channel assigned
+        col_indexes(index) = []; %Removing element
+        
+        %Choose one of the available channels
+        freq_alloc(nextAP) = bestChannel(neighbourlist, nextAP, I, freq_alloc, availableFreqs);
+    end
 end
 end
