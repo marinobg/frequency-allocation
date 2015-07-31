3bc971e26fd4a2dbc8cfa72cd7f8a070a8fbde52
Marino <mabrgr@gmail.com>
2015-07-31 11:28:44 +0200

    Changed the way APs with channel assigned is ignored

--- freq_allocation.m
+++ freq_allocation.m
@@ -15,7 +15,7 @@ availableFreqs = [1, 6, 11];
 Xmax = Size - 2*MC;
 Ymax = Size - 2*MC;
 
-[Px, Py, d, neighbourlist] = createPointsAndDistances(NP, MC, Xmax, Ymax); 
+[Px, Py, d, neighbourlist] = createPointsAndDistances(NP, MC, Xmax, Ymax);
 
 initialPlot(Px, Py, Size, NP)
 
@@ -35,10 +35,8 @@ if alone
     for i = 1:length(alone)
         AP = alone(i);
         freq_alloc(AP) = availableFreqs(randi(numel(availableFreqs))); %Select random channel
-        neighbourlist(:, AP) = []; %Remove the APs neighbourlist, as it is not interesting anymore
     end
 end
-
 
 while length(find(freq_alloc)) < NP
     maxval = max(neighbourlist(NP,:));
@@ -47,22 +45,24 @@ while length(find(freq_alloc)) < NP
     if length(AP) == 1
         freq = chooseFrequency(AP, neighbourlist, I, availableFreqs, freq_alloc); %Finds optimal frequency for AP
         freq_alloc(AP) = freq; %Assign frequency
-        neighbourlist(:, AP) = []; %Remove the APs neighbourlist, as it is not interesting anymore
+        neighbourlist(NP, AP) = 0; %"Remove" the APs neighbourlist, as it is not interesting anymore. Set value to zero to ignore AP
         
-    else %Skriver for at den virker for 2. Kan oppstå flere enn to (liten sjans). MÅ DA SKRIVE OM!
+    else
         invmeters = sumInverseMeters(AP, neighbourlist, I); %Find most critical AP, sum of all inversemeter value for the APs
-    end
-    
-end
-
         
+        while ~isempty(invmeters)
+            [~, index] = max(invmeters); %Finds index of max value in invmeters
+            critAP = AP(index); %Choose the most critical AP of the APs already chosen
+            
+            freq = chooseFrequency(critAP, neighbourlist, I, availableFreqs, freq_alloc); %Finds optimal frequency for AP
+            freq_alloc(critAP) = freq; %Assign frequency
+            neighbourlist(NP, critAP) = 0; %"Remove" the APs neighbourlist, as it is not interesting anymore
+            
+            %Remove value from invmeters and the AP assigned
+            invmeters(index) = [];
+            AP(index) = [];
+        end %while
+        
+    end %if
     
-
-%TODO: FINN EN MÅTE Å LØSE PROBLEMET MED FREKVENSALLOKERINGEN. ANTAR EN HAR
-%OVERORDNET OVERSIKT. FINN MEST KRITISKE PAR, GI DEM EN FREKVENS. DETTE
-%RAPPORTERES INN OG SÅ FJERNES DISSE NODENE FRA NABOLISTENE TIL DE ANDRE
-%NODENE. EVT. SÅ KAN NABOLISTENE TIL DE SOM HAR FÅTT FREKVENS FJERNES
-
-%Fiding the most critical node of the nodes chosen with shortest distance
-%(probably just two) (Assume it is just two for now)
-%invmeters = sumInverseMeters(col_indexes, neighbourlist, I);
+end %while
