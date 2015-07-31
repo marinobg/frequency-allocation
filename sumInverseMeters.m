ff277e54ef9d5587efc14f07dee8839a4f62103f
Marino <mabrgr@gmail.com>
2015-07-31 08:45:30 +0200

    Changed variable name

--- sumInverseMeters.m
+++ sumInverseMeters.m
@@ -1,12 +1,12 @@
-function invmeters = sumInverseMeters(col_indexes, neighbourlist, I)
+function invmeters = sumInverseMeters(AP, neighbourlist, I)
 
 %Fiding the most critical node of the nodes chosen with shortest distance
 %(probably just two)
-invmeters = zeros(1, length(col_indexes));
+invmeters = zeros(1, length(AP));
 for i = 1:length(neighbourlist)
     for j = 1:length(neighbourlist)
-        if any(I(i,j) == col_indexes)
-            index = find(I(i,j) == col_indexes);
+        if any(I(i,j) == AP)
+            index = find(I(i,j) == AP);
             invmeters(index) = invmeters(index) + neighbourlist(i,j);
         end
     end
