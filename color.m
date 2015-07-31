bfb9b264d9b2d6d24c8af05d8435ac2e0e0a6ae6
Marino <mabrgr@gmail.com>
2015-07-31 13:37:01 +0200

    Give each channel a specific color

---
+++ color.m
@@ -0,0 +1,15 @@
+%Checking what channel an AP is assigned to and give it a corresponding
+%color
+
+function col = color(frec_alloc, j)
+switch(frec_alloc(j))
+    case 1
+        col = 'm';
+    case 6
+        col = 'r';
+    case 11
+        col = 'g';
+    otherwise
+        col = 'b';
+end
+end
