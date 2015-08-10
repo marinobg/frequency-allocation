6704b453e87593665f015759ae97b352f5d5c094
Marino <mabrgr@gmail.com>
2015-08-10 08:24:20 +0200

    Added fixed points. For testing

--- createPointsAndDistances.m
+++ createPointsAndDistances.m
@@ -7,6 +7,27 @@ for i=1:NP
     Py(i)=MC+randi(Ymax);
 end %i
 
+% Px(1) = 25;
+% Py(1) = 41;
+% Px(2) = 55;
+% Py(2) = 49;
+% Px(3) = 69;
+% Py(3) = 52;
+% Px(4) = 34;
+% Py(4) = 63;
+% Px(5) = 29;
+% Py(5) = 60;
+% Px(6) = 59;
+% Py(6) = 34;
+% Px(7) = 31;
+% Py(7) = 30;
+% Px(8) = 29;
+% Py(8) = 33;
+% Px(9) = 78;
+% Py(9) = 21;
+% Px(10) = 78;
+% Py(10) = 22;
+
 % assign neighbour lists to points
 d = zeros(NP);
 nlist = d;
