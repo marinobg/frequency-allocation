1cdb25b3f1dd00d4ba00be84651618aceddb09cf
Marino <mabrgr@gmail.com>
2015-08-10 08:26:32 +0200

    Value of NP changed. For testing

--- freq_allocation.m
+++ freq_allocation.m
@@ -1,7 +1,7 @@
 clear all
 
 % Number of APs
-NP = 5;
+NP =10;
 
 % max distance between points
 MC = 20;
@@ -22,9 +22,13 @@ initialPlot(Px, Py, Size, NP)
 
 [neighbourlist,I] = sort(neighbourlist,1,'ascend');
 
+%distributionPlot(neighbourlist, NP) %Plot med fordeling av metrikkene
+
 neighbourlist
 
 I
+
+freq_allocSelfish = selfishAllocation(NP, availableFreqs, neighbourlist, I, Px, Py, Size, d);
 
 freq_alloc = zeros(1, NP); %Vector with the channels each AP get assigned
 
@@ -35,7 +39,7 @@ if alone
     for i = 1:length(alone)
         AP = alone(i);
         freq_alloc(AP) = availableFreqs(randi(numel(availableFreqs))); %Select random channel
-        frequencyPlot(Px, Py, Size, NP, freq_alloc)
+        frequencyPlot(Px, Py, Size, NP, freq_alloc, false)
     end
 end
 
@@ -47,10 +51,11 @@ while length(find(freq_alloc)) < NP
         freq = chooseFrequency(AP, neighbourlist, I, availableFreqs, freq_alloc); %Finds optimal frequency for AP
         freq_alloc(AP) = freq; %Assign frequency
         neighbourlist(NP, AP) = 0; %"Remove" the APs neighbourlist, as it is not interesting anymore. Set value to zero to ignore AP
-        frequencyPlot(Px, Py, Size, NP, freq_alloc)
+        frequencyPlot(Px, Py, Size, NP, freq_alloc, false)
         
     else
         invmeters = sumInverseMeters(AP, neighbourlist, I); %Find most critical AP, sum of all inversemeter value for the APs
+        
         
         while ~isempty(invmeters)
             [~, index] = max(invmeters); %Finds index of max value in invmeters
@@ -64,9 +69,14 @@ while length(find(freq_alloc)) < NP
             invmeters(index) = [];
             AP(index) = [];
             
-            frequencyPlot(Px, Py, Size, NP, freq_alloc)
+            frequencyPlot(Px, Py, Size, NP, freq_alloc, false)
         end %while
         
     end %if
     
 end %while
+
+
+
+%Creating distance plot
+distancePlot(d, freq_alloc, NP, freq_allocSelfish)
