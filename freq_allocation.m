ce995d298328467c4e260f3f31497ba7cb24d0e3
Marino <mabrgr@gmail.com>
2015-08-13 15:47:45 +0200

    Trying to determine node to start with from how many surrounding nodes that node has in its neighbourhood less than a certain radius

--- freq_allocation.m
+++ freq_allocation.m
@@ -3,7 +3,7 @@ clear all
 % Number of APs
 NP = 10;
 
-% max dista5ce between points
+% max distance between points
 MC = 20;
 
 % min distance
@@ -29,7 +29,7 @@ neighbourlist
 I
 
 freq_allocSelfish = selfishAllocation(NP, availableFreqs, neighbourlist, I, Px, Py, Size, d);
-freq_allocSelfish = [1 11 6 6 1 11 6 1 1 11];
+%freq_allocSelfish = [1 6 6 11 1 11 6 1 1 11];
 
 freq_alloc = zeros(1, NP); %Vector with the channels each AP get assigned
 
@@ -57,6 +57,39 @@ end
 % end
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Nodes to begin with are the ones that detect a certain number of nodes
+%inside a certain radius. E.g. 3 nodes inside 20m
+radius = 20; %Detection radius
+nNodes = 5; %Number of nodes required to be inside the radius
+nodes = 1:NP;
+
+while isempty(find(freq_alloc)) %As long as no node have assigned a frequency
+    node = nodes(randi(numel(nodes))); %Pick random node
+    
+    nlist = neighbourlist(:, node); %Get that node's neighbour list
+    detected = length(find(nlist < radius)); %Find number of nodes inside the radius
+    if detected >= nNodes
+        nextAP = node;
+        freq = chooseFrequency(nextAP, neighbourlist, I, availableFreqs, freq_alloc); %Finds optimal frequency for AP
+        freq_alloc(nextAP) = freq; %Assign frequency
+        frequencyPlot(Px, Py, Size, NP, freq_alloc, false)
+        break
+    end
+    
+    nodes(find(node)) = []; %Remove node being checked as it does not fulfill the criteria
+    
+    if isempty(nodes)
+        nodes = 1:NP;
+        nNodes = nNodes - 1;
+    end
+    
+    %Kanskje fjerne noden som er valgt hvis den ikke tilfredstiller?
+end
+    
+
+
+
 
 
 
@@ -65,32 +98,18 @@ end
 %Center of mass is calculated from nodes assigned to a channel
 %Initial, center of mass is calculated from all the nodes
 
-%Initial choice between center of mass or most critical pair of nodes
-
-masscenterX = sum(Px) / NP;
-masscenterY = sum(Py) / NP;
-dist = zeros(1, NP);
-for i = 1:NP
-    dist(i) = sqrt( (Px(i) - masscenterX)^2 + (Py(i) - masscenterY)^2 );
-end
-
-[minDistMass, ~] = min(dist);
-
-mindist = min(neighbourlist(NP,:)); %Minimum distance
-
-%Choose AP after smallest distance between two nodes or smallest distance
-%to center of mass
-if minDistMass < mindist
-    [~, AP] = min(dist);
-else
-    AP = find(neighbourlist(NP,:) == mindist); %Closest APs in the topology
-end
-
-for nextAP = AP
-    freq = chooseFrequency(nextAP, neighbourlist, I, availableFreqs, freq_alloc); %Finds optimal frequency for AP
-    freq_alloc(nextAP) = freq; %Assign frequency
-    frequencyPlot(Px, Py, Size, NP, freq_alloc, false)
-end
+% masscenterX = sum(Px) / NP;
+% masscenterY = sum(Py) / NP;
+% dist = zeros(1, NP);
+% for i = 1:NP
+%     dist(i) = sqrt( (Px(i) - masscenterX)^2 + (Py(i) - masscenterY)^2 );
+% end
+% 
+% [~, nextAP] = min(dist);
+% 
+% freq = chooseFrequency(nextAP, neighbourlist, I, availableFreqs, freq_alloc); %Finds optimal frequency for AP
+% freq_alloc(nextAP) = freq; %Assign frequency
+% frequencyPlot(Px, Py, Size, NP, freq_alloc, false)
 
 while length(find(freq_alloc)) < NP
     nodes = find(freq_alloc); %Find nodes assigned to a frequency
@@ -104,16 +123,8 @@ while length(find(freq_alloc)) < NP
         dist(i) = sqrt( (Px(freeNodes(i)) - masscenterX)^2 + (Py(freeNodes(i)) - masscenterY)^2 );
     end
     
-    [minDistMass, index] = min(dist);
-    
-    indexes = find(freq_alloc);
-    [~, mindist] = findSmallestDist(d, indexes);
-    
-    if minDistMass < mindist
-        nextAP = freeNodes(index);
-    else
-        [nextAP, ~] = findSmallestDist(d, indexes);
-    end
+    [~, index] = min(dist);
+    nextAP = freeNodes(index);
     
     freq = chooseFrequency(nextAP, neighbourlist, I, availableFreqs, freq_alloc); %Finds optimal frequency for AP
     freq_alloc(nextAP) = freq; %Assign frequency
