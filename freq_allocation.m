11017b76685d6d259ac70a66e584b07f50947cc2
Marino <mabrgr@gmail.com>
2015-08-14 12:16:28 +0200

    First node(s) are chosen by looking at how many nodes they can detect inside a certain radius. Closest distance to another node is also important. This is only if the allocation should start from one place

--- freq_allocation.m
+++ freq_allocation.m
@@ -29,7 +29,7 @@ neighbourlist
 I
 
 freq_allocSelfish = selfishAllocation(NP, availableFreqs, neighbourlist, I, Px, Py, Size, d);
-%freq_allocSelfish = [1 6 6 11 1 11 6 1 1 11];
+%freq_allocSelfish = [6 11 1 11 6];
 
 freq_alloc = zeros(1, NP); %Vector with the channels each AP get assigned
 
@@ -60,71 +60,69 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Nodes to begin with are the ones that detect a certain number of nodes
 %inside a certain radius. E.g. 3 nodes inside 20m
-radius = 20; %Detection radius
-nNodes = 5; %Number of nodes required to be inside the radius
+radius = (NP / ((max(Px) - min(Px)) * (max(Py) - min(Py))))* ((Xmax*Ymax)/2) ; %Detection radius, use node denisty to calculate
+nNodes = ceil(NP / 2); %Number of nodes required to be inside the radius
 nodes = 1:NP;
+mindist = inf; %Minimum distance in a neighbour list that fulfill the criteria for radius and nNodes
 
 while isempty(find(freq_alloc)) %As long as no node have assigned a frequency
     node = nodes(randi(numel(nodes))); %Pick random node
     
     nlist = neighbourlist(:, node); %Get that node's neighbour list
     detected = length(find(nlist < radius)); %Find number of nodes inside the radius
-    if detected >= nNodes
+    if detected >= nNodes && neighbourlist(NP, node) < mindist
+        mindist = neighbourlist(NP, node);
         nextAP = node;
-        freq = chooseFrequency(nextAP, neighbourlist, I, availableFreqs, freq_alloc); %Finds optimal frequency for AP
-        freq_alloc(nextAP) = freq; %Assign frequency
-        frequencyPlot(Px, Py, Size, NP, freq_alloc, false)
-        break
+    elseif detected >= nNodes && neighbourlist(NP, node) == mindist
+        nextAP(end + 1) = node; %Append to nextAP list
     end
     
-    nodes(find(node)) = []; %Remove node being checked as it does not fulfill the criteria
+    nodes(find(nodes == node)) = []; %Remove node being checked as it does not fulfill the criteria
     
+    %If all nodes are checked and at least one fulfill the criteria
+    if isempty(nodes) && mindist < inf
+        for AP = nextAP
+            freq = chooseFrequency(AP, neighbourlist, I, availableFreqs, freq_alloc); %Finds optimal frequency for AP
+            freq_alloc(AP) = freq; %Assign frequency
+            frequencyPlot(Px, Py, Size, NP, freq_alloc, false)
+        end
+        break
+    end
+        
+    
+    %If all nodes are checked and none of them fulfill the criteria
     if isempty(nodes)
         nodes = 1:NP;
-        nNodes = nNodes - 1;
+        nNodes = nNodes - 1; %Required number of nodes to be detected is one less
+        if nNodes <= 1 %If no neighbour list can find two or more nodes inside the radius criteria
+            %Back to start with nNodes, but with larger radius
+            radius = radius + 5;
+            nNodes = ceil(NP / 2);
+        end
     end
     
     %Kanskje fjerne noden som er valgt hvis den ikke tilfredstiller?
 end
-    
+   
 
 
-
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Test to see if center of mass is the correct approach. 
-%Center of mass is calculated from nodes assigned to a channel
-%Initial, center of mass is calculated from all the nodes
-
-% masscenterX = sum(Px) / NP;
-% masscenterY = sum(Py) / NP;
-% dist = zeros(1, NP);
-% for i = 1:NP
-%     dist(i) = sqrt( (Px(i) - masscenterX)^2 + (Py(i) - masscenterY)^2 );
-% end
-% 
-% [~, nextAP] = min(dist);
-% 
-% freq = chooseFrequency(nextAP, neighbourlist, I, availableFreqs, freq_alloc); %Finds optimal frequency for AP
-% freq_alloc(nextAP) = freq; %Assign frequency
-% frequencyPlot(Px, Py, Size, NP, freq_alloc, false)
 
 while length(find(freq_alloc)) < NP
     nodes = find(freq_alloc); %Find nodes assigned to a frequency
+    
+    %Calculate center of mass from nodes assigned to frequency
     masscenterX = sum(Px(nodes)) / length(nodes);
     masscenterY = sum(Py(nodes)) / length(nodes);
     dist = zeros(1, NP-length(nodes));
     
     freeNodes = find(freq_alloc == 0); %Nodes not assigned to a frequency
     
-    for i = 1:length(dist)
+    for i = 1:length(dist) %Distances from nodes without frequency to center of mass
         dist(i) = sqrt( (Px(freeNodes(i)) - masscenterX)^2 + (Py(freeNodes(i)) - masscenterY)^2 );
     end
     
     [~, index] = min(dist);
-    nextAP = freeNodes(index);
+    nextAP = freeNodes(index); %Node with smallest distance to center of mass
     
     freq = chooseFrequency(nextAP, neighbourlist, I, availableFreqs, freq_alloc); %Finds optimal frequency for AP
     freq_alloc(nextAP) = freq; %Assign frequency
@@ -134,12 +132,9 @@ end
 
 
 
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-
-
-
 
 % while length(find((freq_alloc))) < NP %Assign the other APs to a channel
 %     indexes = find(freq_alloc); %Find links that are assigned to a frequency
@@ -149,48 +144,6 @@ end
 %     freq_alloc(nextAP) = freq; %Assign frequency
 %     frequencyPlot(Px, Py, Size, NP, freq_alloc, false)
 % end
-
-
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%Version 1 of frequency allocation with neighbourlists
-
-% while length(find(freq_alloc)) < NP
-%     maxval = max(neighbourlist(NP,:));
-%     AP = find(neighbourlist(NP,:) == maxval);
-%     
-%     if length(AP) == 1
-%         freq = chooseFrequency(AP, neighbourlist, I, availableFreqs, freq_alloc); %Finds optimal frequency for AP
-%         freq_alloc(AP) = freq; %Assign frequency
-%         neighbourlist(NP, AP) = 0; %"Remove" the APs neighbourlist, as it is not interesting anymore. Set value to zero to ignore AP
-%         frequencyPlot(Px, Py, Size, NP, freq_alloc, false)
-%         
-%     else
-%         invmeters = sumInverseMeters(AP, neighbourlist, I); %Find most critical AP, sum of all inversemeter value for the APs
-%         
-%         
-%         while ~isempty(invmeters)
-%             [~, index] = max(invmeters); %Finds index of max value in invmeters
-%             critAP = AP(index); %Choose the most critical AP of the APs already chosen
-%             
-%             freq = chooseFrequency(critAP, neighbourlist, I, availableFreqs, freq_alloc); %Finds optimal frequency for AP
-%             freq_alloc(critAP) = freq; %Assign frequency
-%             neighbourlist(NP, critAP) = 0; %"Remove" the APs neighbourlist, as it is not interesting anymore
-%             
-%             %Remove value from invmeters and the AP assigned
-%             invmeters(index) = [];
-%             AP(index) = [];
-%             
-%             frequencyPlot(Px, Py, Size, NP, freq_alloc, false)
-%         end %while
-%         
-%     end %if
-%     
-% end %while
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 
