653446dee1441a81a5c2944f32e10f2544b6cbb9
Marino <mabrgr@gmail.com>
2015-08-14 13:57:57 +0200

    Function to check if a node in the neighbour list to another node has already been allowed to choose a channel

---
+++ checkNeighbourlist.m
@@ -0,0 +1,26 @@
+function nodeAssigned = checkNeighbourlist(nlist, I, node, nextAP)
+%Funtion that check if a node have a node in its neighbour list that
+%already is allowed to choose a frequency
+
+
+interfering = I(:, node);
+for i = 1:length(nlist)
+    if nlist(i) == inf
+        interfering(i) = 0; %Mark which APs to remove
+    end
+end
+
+interfering = interfering(find(interfering)); %Removing APs that are not detected
+
+for AP = nextAP
+    for i = 1:length(interfering)
+        if interfering(i) == AP
+            nodeAssigned = false; %This node can not assign a frequency in the initial phase
+            return
+        end
+    end
+end
+
+nodeAssigned = true;
+
+end
