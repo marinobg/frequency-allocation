7bd5fff47fb5c209ae66f6bd99eb21d2da9072c3
Marino <mabrgr@gmail.com>
2015-08-18 14:23:52 +0200

    Testing

--- createPointsAndDistances.m
+++ createPointsAndDistances.m
@@ -2,52 +2,52 @@ function [Px, Py, d, nlist] = createPointsAndDistances(NP, MC, Xmax, Ymax)
 Px = zeros(1, NP);
 Py = zeros(1, NP);
 
-% for i=1:NP
-%     Px(i)=MC+randi(Xmax);
-%     Py(i)=MC+randi(Ymax);
-% end %i
+Creating random points
+for i=1:NP
+    Px(i)=MC+randi(Xmax);
+    Py(i)=MC+randi(Ymax);
+end %i
 
-% Px(1) = 39;
-% Py(1) = 70;
-% Px(2) = 54;
-% Py(2) = 24;
-% Px(3) = 36;
-% Py(3) = 56;
-% Px(4) = 49;
-% Py(4) = 32;
-% Px(5) = 35;
-% Py(5) = 67;
-% Px(6) = 58;
-% Py(6) = 29;
-% Px(7) = 63;
-% Py(7) = 45;
-% Px(8) = 48;
-% Py(8) = 63;
-% Px(9) = 45;
-% Py(9) = 21;
-% Px(10) = 25;
-% Py(10) = 56;
+% Px(1) = 21;
+% Py(1) = 24;
+% Px(2) = 76;
+% Py(2) = 75;
+% Px(3) = 50;
+% Py(3) = 38;
+% Px(4) = 39;
+% Py(4) = 53;
+% Px(5) = 31;
+% Py(5) = 35;
+% Px(6) = 28;
+% Py(6) = 76;
+% Px(7) = 28;
+% Py(7) = 70;
+% Px(8) = 51;
+% Py(8) = 51;
+% Px(9) = 68;
+% Py(9) = 29;
+% Px(10) = 31;
+% Py(10) = 71;
 
-Px(1) = 41;
-Py(1) = 72;
-Px(2) = 51;
-Py(2) = 72;
-Px(3) = 44;
-Py(3) = 62;
-Px(4) = 49;
-Py(4) = 77;
-Px(5) = 47;
-Py(5) = 65;
+% Px(1) = 58;
+% Py(1) = 60;
+% Px(2) = 25;
+% Py(2) = 33;
+% Px(3) = 46;
+% Py(3) = 24;
+% Px(4) = 40;
+% Py(4) = 55;
+% Px(5) = 55;
+% Py(5) = 48;
+
 
 % assign neighbour lists to points
-%d = zeros(NP);
 d = ones(NP) * inf;
 nlist = d;
 for i = 1:NP
     for j = 1:NP
         d(i,j) = sqrt((Px(i)-Px(j))^2 + (Py(i)-Py(j))^2);
-        if d(i,j) < MC*2 && i ~= j
-            %nlist(i,j) = 1 / d(i,j);
+        if i ~= j %&& d(i,j) < MC*2 %Add d(i,j)<MC*2 to put limit on how big the radius of detection for each node should be
             nlist(i,j) = d(i,j);
         end
     end %j
