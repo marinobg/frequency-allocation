b19cc568e731e90a914ae1161d8761ffba1b058d
Marino <mabrgr@gmail.com>
2015-08-18 08:26:30 +0200

    Added comment

--- initialPlot.m
+++ initialPlot.m
@@ -1,9 +1,12 @@
 function initialPlot(Px, Py, Size, NP)
 
+%Creates a plot of the topology before any channels are assigned
+
 figure(1)
 plot(Px,Py,'x', 'markersize', 8)
 axis([ 0 Size 0 Size])
 lables = cellstr(num2str([1:NP]'));
 text(Px, Py, lables, 'VerticalAlignment','bottom', 'HorizontalAlignment','right')
+%text is plotting a label on each node in the topology (numbers from 1 to NP)
 
 end
